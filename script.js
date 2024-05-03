694b023235530fcf72c5dbc3258d68c4eaa8e7e3
Chris <chris79gp@outlook.com>
2024-05-03 20:34:09 +0100

    feat: toggle .active class on navigation corresponding to currently visible section

--- script.js
+++ script.js
@@ -19,3 +19,24 @@ window.addEventListener("scroll", function () {
 		navElement.classList.remove("floating-nav");
 	}
 });
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+const toggleActiveLink = function () {
+	const scrollPosition = window.scrollY;
+
+	document.querySelectorAll("section").forEach(section => {
+		const sectionTop = section.offsetTop - 42;
+		const sectionHeight = section.clientHeight;
+
+		if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+			const navLink = document.querySelector(`[href="#${section.id}"]`);
+
+			navLinks.forEach(link => link.classList.remove("active"));
+
+			navLink.classList.add("active");
+		}
+	});
+};
+
+window.addEventListener("scroll", toggleActiveLink);
