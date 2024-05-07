6f1c17de1c645abe298397a502f32d21e3502073
Chris <chris79gp@outlook.com>
2024-05-07 22:14:10 +0100

    build: abstract header markup to Header.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import Header from "./Header.jsx";
+
 import "../../script.js";
 import "../styles/App.css";
 
@@ -5,30 +7,7 @@ function App() {
 	return (
 		<>
 			<section id="hero">
-				<header id="nav">
-					<nav>
-						<ul>
-							<a className="nav-link active" href="#hero">
-								<li>Home</li>
-							</a>
-							<a className="nav-link" href="#about">
-								<li>About</li>
-							</a>
-							<a className="nav-link" href="#location">
-								<li>Location</li>
-							</a>
-							<a className="nav-link" href="#gallery">
-								<li>Gallery</li>
-							</a>
-							<a className="nav-link" href="#reviews">
-								<li>Reviews</li>
-							</a>
-							<a className="nav-link" href="#booking">
-								<li>Booking</li>
-							</a>
-						</ul>
-					</nav>
-				</header>
+				<Header />
 				<div className="hero-container">
 					<h1>Owl Cottage</h1>
 					<p>
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+export default function Header() {
+	return (
+		<header id="nav">
+			<nav>
+				<ul>
+					<a className="nav-link active" href="#hero">
+						<li>Home</li>
+					</a>
+					<a className="nav-link" href="#about">
+						<li>About</li>
+					</a>
+					<a className="nav-link" href="#location">
+						<li>Location</li>
+					</a>
+					<a className="nav-link" href="#gallery">
+						<li>Gallery</li>
+					</a>
+					<a className="nav-link" href="#reviews">
+						<li>Reviews</li>
+					</a>
+					<a className="nav-link" href="#booking">
+						<li>Booking</li>
+					</a>
+				</ul>
+			</nav>
+		</header>
+	);
+}
