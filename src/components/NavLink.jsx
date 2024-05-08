10863919c5a79896b6556480ae86f49c2cc43246
Chris <chris79gp@outlook.com>
2024-05-08 14:20:21 +0100

    refactor: abstract styling for NavLink components from Header.css to NavLink.css

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import "../styles/NavLink.css";
 
 export default function NavLink({ href, isActive = false }) {
 	const linkTitle = href.charAt(1).toUpperCase() + href.slice(2);
