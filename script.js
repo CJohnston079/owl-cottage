6e368678fd8f9c6ab73908908689418dc35805f4
Chris <chris79gp@outlook.com>
2024-05-08 16:24:59 +0100

    refactor: migrate logic for toggling .active class on .nav-link anchors from script.js to Header.jsx

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-const navLinks = document.querySelectorAll(".nav-link");
-
-const toggleActiveLink = function () {
-	const scrollPosition = window.scrollY;
-
-	document.querySelectorAll("section").forEach(section => {
-		const sectionTop = section.offsetTop - 42;
-		const sectionHeight = section.clientHeight;
-
-		if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
-			const navLink = document.querySelector(`[href="#${section.id}"]`);
-
-			navLinks.forEach(link => link.classList.remove("active"));
-
-			navLink.classList.add("active");
-		}
-	});
-};
-
-window.addEventListener("scroll", toggleActiveLink);
-
 const sections = document.querySelectorAll("section");
 
 for (let i = 2; i < sections.length; i += 2) {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import "../styles/Header.css";
 
 export default function Header() {
 	const [isFloating, setIsFloating] = useState(false);
+	const [activeSection, setActiveSection] = useState("#home");
 
 	const toggleFloatingNav = () => {
 		const boundingElement = document.querySelector("#home").nextSibling;
@@ -12,18 +13,33 @@ export default function Header() {
 		setIsFloating(distanceFromBoundingElement <= 0);
 	};
 
+	const toggleActiveSection = () => {
+		const scrollPosition = window.scrollY;
+		const sections = document.querySelectorAll("section");
+
+		sections.forEach(section => {
+			const sectionTop = section.offsetTop - 42;
+			const sectionHeight = section.clientHeight;
+
+			if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+				setActiveSection(section.id);
+			}
+		});
+	};
+
 	window.addEventListener("scroll", toggleFloatingNav);
+	window.addEventListener("scroll", toggleActiveSection);
 
 	return (
 		<header id="nav" className={isFloating ? "floating-nav" : ""}>
 			<nav>
 				<ul>
-					<NavLink href="#home" isActive={true} />
-					<NavLink href="#about" />
-					<NavLink href="#location" />
-					<NavLink href="#gallery" />
-					<NavLink href="#reviews" />
-					<NavLink href="#booking" />
+					<NavLink href="#home" isActive={activeSection === "home"} />
+					<NavLink href="#about" isActive={activeSection === "about"} />
+					<NavLink href="#location" isActive={activeSection === "location"} />
+					<NavLink href="#gallery" isActive={activeSection === "gallery"} />
+					<NavLink href="#reviews" isActive={activeSection === "reviews"} />
+					<NavLink href="#booking" isActive={activeSection === "booking"} />
 				</ul>
 			</nav>
 		</header>
