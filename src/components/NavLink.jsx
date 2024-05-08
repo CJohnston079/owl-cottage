ada9c83dfea128c5580abb1aafdcac063c0e67e7
Chris <chris79gp@outlook.com>
2024-05-08 12:52:51 +0100

    feat: add NavLink.jsx

---
+++ src/components/NavLink.jsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+
+export default function NavLink({ href, isActive = false }) {
+	const linkTitle = href.charAt(1).toUpperCase() + href.slice(2);
+
+	return (
+		<a className={`nav-link ${isActive && "active"}`} href={href}>
+			<li>{linkTitle}</li>
+		</a>
+	);
+}
+
+NavLink.propTypes = {
+	href: PropTypes.string,
+	isActive: PropTypes.bool,
+};
