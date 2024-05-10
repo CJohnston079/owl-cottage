b275ba495388bd010069704105075109baf42c0e
Chris <chris79gp@outlook.com>
2024-05-10 15:49:44 +0100

    feat: limit reviews to be rendered at greater than 140 characters and less than 210 characters

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -4,18 +4,22 @@ import Section from "./Section";
 import reviews from "../data/reviews.json";
 
 export default function Testimonials() {
+	const shortReviews = reviews.filter(review => {
+		return review.content.length > 140 && review.content.length <= 210;
+	});
+
 	const randomReviewIds = function () {
 		const randomNumsSet = new Set();
 
 		while (randomNumsSet.size < 3) {
-			randomNumsSet.add(Math.floor(Math.random() * reviews.length));
+			randomNumsSet.add(Math.floor(Math.random() * shortReviews.length));
 		}
 
 		return Array.from(randomNumsSet);
 	};
 
 	const testimonials = randomReviewIds().map(reviewId => {
-		const review = reviews[reviewId];
+		const review = shortReviews[reviewId];
 
 		return (
 			<Testimonial
