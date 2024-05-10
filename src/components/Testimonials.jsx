2b4ed744a18d2b608301e9b9767ec11c20c86177
Chris <chris79gp@outlook.com>
2024-05-10 15:16:15 +0100

    feat: Testimonials now renders three random reviews selected from reviews.json

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,33 +1,30 @@
 import FlexContainer from "./FlexContainer";
 import Testimonial from "./Testimonial";
 import Section from "./Section";
+import reviews from "../data/reviews.json";
 
 export default function Testimonials() {
+	const randomReviewIds = Array.from({ length: 3 }, () =>
+		Math.floor(Math.random() * reviews.length)
+	);
+
+	const testimonials = randomReviewIds.map(reviewId => {
+		const review = reviews[reviewId];
+
+		return (
+			<Testimonial
+				key={reviewId}
+				review={{
+					content: review.content,
+					author: review.name,
+				}}
+			/>
+		);
+	});
+
 	return (
 		<Section sectionId="testimonials">
-			<FlexContainer>
-				<Testimonial
-					review={{
-						content:
-							"My second stay at Owl Cottage. Such a lovely property, good base for lots of exploring. Will definitely be back.",
-						author: "Kelly",
-					}}
-				/>
-				<Testimonial
-					review={{
-						content:
-							"Beautiful house in a great location. Very spacious and has everything you need for a few days away. Great garden, which is fully enclosed for the dog too.",
-						author: "Erin",
-					}}
-				/>
-				<Testimonial
-					review={{
-						content:
-							"Owl Cottage is a very cosy comfortable place to stay and a great base to explore and enjoy the local area. Loads to do and see locally. Highly recommended!",
-						author: "Tracy",
-					}}
-				/>
-			</FlexContainer>{" "}
+			<FlexContainer>{testimonials}</FlexContainer>
 		</Section>
 	);
 }
