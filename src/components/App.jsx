75d308bc19c1746f8ad25ad34ad49a7023cffcca
Chris <chris79gp@outlook.com>
2024-05-12 13:53:22 +0100

    refactor: abstract code for #location to Location.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import NavBar from "./NavBar.jsx";
 import Hero from "./Hero.jsx";
+import Location from "./Location.jsx";
 import Section from "./Section.jsx";
 import Paragraphs from "./Paragraphs.jsx";
 import Testimonials from "./Testimonials.jsx";
-import FlexContainer from "./FlexContainer.jsx";
 import copy from "../data/copy.json";
 import "../styles/App.css";
 
@@ -42,46 +42,7 @@ function App() {
 					</figure>
 				</div>
 			</Section>
-			<Section sectionId="location">
-				<Paragraphs paragraphs={copy.location} />
-				<h2>Points of interest</h2>
-				<FlexContainer>
-					<figure className="img-with-caption">
-						<img src="./src/assets/images/loch-ness.jpeg" alt="Urquhart Castle, Loch Ness" />
-						<figcaption>
-							<h3>Loch Ness</h3>
-							<p>
-								The world-famous Loch Ness is just three miles away. Pictured here is 13th Century
-								Castle Urquhart.
-							</p>
-						</figcaption>
-					</figure>
-					<figure className="img-with-caption">
-						<img
-							src="./src/assets/images/fort-augustus.webp"
-							alt="Fort Augustus, Inverness-shire"
-						/>
-						<figcaption>
-							<h3>Fort Augustus</h3>
-							<p>
-								Nearby Fort Augustus has a good range of local shops and attractions, cycle hire and
-								boat trips on Loch Ness.
-							</p>
-						</figcaption>
-					</figure>
-					<figure className="img-with-caption">
-						<img src="./src/assets/images/loch-oich.webp" alt="Loch Oich shore" />
-						<figcaption>
-							<h3>Loch Oich</h3>
-							<p>
-								Beautiful Loch Oich is just a five minute drive away - join the Great Glen Way here
-								and enjoy some beautiful walks, an excellent cycle route, or even a wild swim for
-								the brave.
-							</p>
-						</figcaption>
-					</figure>
-				</FlexContainer>
-			</Section>
+			<Location />
 			<Testimonials />
 			<Section sectionId="booking"></Section>
 			<footer>
---
+++ src/components/Location.jsx
@@ -0,0 +1,47 @@
+import FlexContainer from "./FlexContainer";
+import Paragraphs from "./Paragraphs";
+import Section from "./Section";
+import copy from "../data/copy.json";
+// import "../styles/Hero.css";
+
+export default function Location() {
+	return (
+		<Section sectionId="location">
+			<Paragraphs paragraphs={copy.location} />
+			<h2>Points of interest</h2>
+			<FlexContainer>
+				<figure className="img-with-caption">
+					<img src="./src/assets/images/loch-ness.jpeg" alt="Urquhart Castle, Loch Ness" />
+					<figcaption>
+						<h3>Loch Ness</h3>
+						<p>
+							The world-famous Loch Ness is just three miles away. Pictured here is 13th Century
+							Castle Urquhart.
+						</p>
+					</figcaption>
+				</figure>
+				<figure className="img-with-caption">
+					<img src="./src/assets/images/fort-augustus.webp" alt="Fort Augustus, Inverness-shire" />
+					<figcaption>
+						<h3>Fort Augustus</h3>
+						<p>
+							Nearby Fort Augustus has a good range of local shops and attractions, cycle hire and
+							boat trips on Loch Ness.
+						</p>
+					</figcaption>
+				</figure>
+				<figure className="img-with-caption">
+					<img src="./src/assets/images/loch-oich.webp" alt="Loch Oich shore" />
+					<figcaption>
+						<h3>Loch Oich</h3>
+						<p>
+							Beautiful Loch Oich is just a five minute drive away - join the Great Glen Way here
+							and enjoy some beautiful walks, an excellent cycle route, or even a wild swim for the
+							brave.
+						</p>
+					</figcaption>
+				</figure>
+			</FlexContainer>
+		</Section>
+	);
+}
