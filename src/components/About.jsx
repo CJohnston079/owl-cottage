1915a59385a769fdfd3b89dcc197cd8a3c753bda
Chris <chris79gp@outlook.com>
2024-05-12 21:18:10 +0100

    refactor: abstract jsx and css for #about section to About.jsx and About.css respectively

---
+++ src/components/About.jsx
@@ -0,0 +1,39 @@
+import Paragraphs from "./Paragraphs";
+import Section from "./Section";
+import copy from "../data/copy.json";
+import "../styles/About.css";
+
+export default function About() {
+	return (
+		<Section sectionId="about">
+			<Paragraphs paragraphs={copy.about} />
+			<h3>Amenities</h3>
+			<div className="img-grid">
+				<figure className="icon-with-caption">
+					<img src="./src/assets/icons/bed.svg" alt="" />
+					<figcaption>2 bedrooms</figcaption>
+				</figure>
+				<figure className="icon-with-caption">
+					<img src="./src/assets/icons/pets.svg" alt="" />
+					<figcaption>Pet friendly</figcaption>
+				</figure>
+				<figure className="icon-with-caption">
+					<img src="./src/assets/icons/car.svg" alt="" />
+					<figcaption>Driveway</figcaption>
+				</figure>
+				<figure className="icon-with-caption">
+					<img src="./src/assets/icons/view.svg" alt="" />
+					<figcaption>Mountain view</figcaption>
+				</figure>
+				<figure className="icon-with-caption">
+					<img src="./src/assets/icons/wifi.svg" alt="" />
+					<figcaption>Free wifi</figcaption>
+				</figure>
+				<figure className="icon-with-caption">
+					<img src="./src/assets/icons/tv.svg" alt="" />
+					<figcaption>55-inch TV with Netflix</figcaption>
+				</figure>
+			</div>
+		</Section>
+	);
+}
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,9 @@
 import NavBar from "./NavBar.jsx";
 import Hero from "./Hero.jsx";
+import About from "./About.jsx";
 import Location from "./Location.jsx";
 import Section from "./Section.jsx";
-import Paragraphs from "./Paragraphs.jsx";
 import Testimonials from "./Testimonials.jsx";
-import copy from "../data/copy.json";
 import "../styles/App.css";
 
 function App() {
@@ -12,36 +11,7 @@ function App() {
 		<>
 			<NavBar />
 			<Hero />
-			<Section sectionId="about">
-				<Paragraphs paragraphs={copy.about} />
-				<h3>Amenities</h3>
-				<div className="img-grid">
-					<figure className="icon-with-caption">
-						<img src="./src/assets/icons/bed.svg" alt="" />
-						<figcaption>2 bedrooms</figcaption>
-					</figure>
-					<figure className="icon-with-caption">
-						<img src="./src/assets/icons/pets.svg" alt="" />
-						<figcaption>Pet friendly</figcaption>
-					</figure>
-					<figure className="icon-with-caption">
-						<img src="./src/assets/icons/car.svg" alt="" />
-						<figcaption>Driveway</figcaption>
-					</figure>
-					<figure className="icon-with-caption">
-						<img src="./src/assets/icons/view.svg" alt="" />
-						<figcaption>Mountain view</figcaption>
-					</figure>
-					<figure className="icon-with-caption">
-						<img src="./src/assets/icons/wifi.svg" alt="" />
-						<figcaption>Free wifi</figcaption>
-					</figure>
-					<figure className="icon-with-caption">
-						<img src="./src/assets/icons/tv.svg" alt="" />
-						<figcaption>55-inch TV with Netflix</figcaption>
-					</figure>
-				</div>
-			</Section>
+			<About />
 			<Location />
 			<Testimonials />
 			<Section sectionId="booking"></Section>
