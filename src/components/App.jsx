49c26da678b155a44fcf7e40721b3ee43e5a62ec
Chris <chris79gp@outlook.com>
2024-05-14 21:36:01 +0100

    feat: add Booking.jsx Booking component returns a Section containg a form <form> element is split into "Select dates", "You details" and "Confirm booking" sections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import NavBar from "./NavBar.jsx";
 import Hero from "./Hero.jsx";
 import About from "./About.jsx";
 import Location from "./Location.jsx";
-import Section from "./Section.jsx";
+import Booking from "./Booking.jsx";
 import Testimonials from "./Testimonials.jsx";
 import "../styles/App.css";
 
@@ -14,7 +14,8 @@ function App() {
 			<About />
 			<Location />
 			<Testimonials />
-			<Section sectionId="booking"></Section>
+			{/* <Section sectionId="booking"></Section> */}
+			<Booking />
 			<footer>
 				<p>Copyright © 2024 Owl Cottage. All rights reserved.</p>
 			</footer>
---
+++ src/components/Booking.jsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import Button from "./Button";
+import Section from "./Section";
+
+export default function Booking() {
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		guests: "",
+		startDate: "",
+		endDate: "",
+	});
+
+	const handleInput = e => {
+		const { name, value } = e.target;
+		setFormData({ ...formData, [name]: value });
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		console.log("Booking submitted:", formData);
+		// Send form data to back-end
+		// Reset form fields
+	};
+
+	const { name, email, guests, startDate, endDate } = formData;
+
+	return (
+		<Section sectionId={"booking"}>
+			<form onSubmit={handleSubmit}>
+				<div className="form-section">
+					<h3>1. Select dates</h3>
+					<ul className="section-fields">
+						<li>
+							<label htmlFor="startDate">Start date:</label>
+							<input
+								type="date"
+								id="startDate"
+								name="startDate"
+								value={startDate}
+								onChange={handleInput}
+								required
+							/>
+						</li>
+						<li>
+							<label htmlFor="endDate">End date:</label>
+							<input
+								type="date"
+								id="endDate"
+								name="endDate"
+								value={endDate}
+								onChange={handleInput}
+								required
+							/>
+						</li>
+					</ul>
+				</div>
+				<div className="form-section">
+					<h3>2. Your details</h3>
+					<ul className="section-fields">
+						<li>
+							<label htmlFor="name">Full name:</label>
+							<input
+								type="text"
+								id="name"
+								name="name"
+								value={name}
+								onChange={handleInput}
+								required
+							/>
+						</li>
+						<li>
+							<label htmlFor="email">Email:</label>
+							<input
+								type="email"
+								id="email"
+								name="email"
+								value={email}
+								onChange={handleInput}
+								required
+							/>
+						</li>
+						<li>
+							<label htmlFor="guests">Guests</label>
+							<input
+								type="number"
+								id="guests"
+								name="guests"
+								max={4}
+								value={guests}
+								onChange={handleInput}
+								required
+							/>
+						</li>
+					</ul>
+				</div>
+				<div className="form-section">
+					<h3>3. Confirm reservation</h3>
+					<ul className="section-fields">
+						<li>Name: {name}</li>
+						<li>Email: {email}</li>
+						<li>Guests: {guests}</li>
+						<li>Start date: {startDate}</li>
+						<li>End date: {endDate}</li>
+					</ul>
+					<Button type="submit" style="emphasis">
+						Confirm booking
+					</Button>
+				</div>
+			</form>
+		</Section>
+	);
+}
