46d611094905cef2e61ceb0c874b452f0ed39b53
Chris <chris79gp@outlook.com>
2024-05-15 22:00:31 +0100

    fix: add "value" prop to LabelledInput to correctly display the <input> value

--- src/components/LabelledField.jsx
+++ src/components/LabelledField.jsx
@@ -1,17 +1,17 @@
 import PropTypes from "prop-types";
 
-export default function LabelledField({ type, labelContent, inputName, handleInput }) {
+export default function LabelledField({ type, labelContent, inputName, value, handleInput }) {
 	return (
 		<li className="field">
 			<label className="field-label" htmlFor={inputName}>
-				{labelContent}
+				{labelContent + ":"}
 			</label>
 			<input
 				className="field-input"
 				type={type}
 				id={inputName}
 				name={inputName}
-				value={inputName}
+				value={value}
 				onChange={handleInput}
 				required
 			/>
@@ -23,5 +23,6 @@ LabelledField.propTypes = {
 	type: PropTypes.string,
 	labelContent: PropTypes.string,
 	inputName: PropTypes.string,
+	value: PropTypes.string,
 	handleInput: PropTypes.func,
 };
