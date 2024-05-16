ab6de0740a06d50611361abc0fd14eb9c51f2f10
Chris <chris79gp@outlook.com>
2024-05-16 17:52:46 +0100

    feat: add FormSection component FormSection returns a .section-fields <ul> of LabelledInputs FormSection has props "sectionFields" and "handleInput" "sectionFields" is an array of objects that contain the props for each LabelledInput to be rendered handleInput is a callback function to handle the onChange event in the <input> elements

---
+++ src/components/FormSection.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import LabelledField from "./LabelledField";
+
+export default function FormSection({ sectionFields, handleInput }) {
+	const fields = sectionFields.map((field, index) => (
+		<LabelledField
+			key={index}
+			type={field.type}
+			labelContent={field.labelContent}
+			inputName={field.inputName}
+			value={sectionFields[field.inputName]}
+			handleInput={handleInput}
+		/>
+	));
+
+	return <ul className="section-fields">{fields}</ul>;
+}
+
+FormSection.propTypes = {
+	sectionFields: PropTypes.arrayOf(
+		PropTypes.shape({
+			labelContent: PropTypes.string,
+			inputName: PropTypes.string,
+			type: PropTypes.string,
+		})
+	),
+	handleInput: PropTypes.func,
+};
