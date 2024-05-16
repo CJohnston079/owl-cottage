cbe4597fdb929619491fb9d71a3cdf91e823d2f3
Chris <chris79gp@outlook.com>
2024-05-16 20:59:33 +0100

    refactor: replace .section-fields <ul> in Form.jsx with Fields component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 import Button from "./Button";
-import LabelledField from "./LabelledField";
+import Fields from "./Fields";
 import "../styles/Form.css";
 
 export default function Form({ fieldsData, children }) {
@@ -20,20 +20,9 @@ export default function Form({ fieldsData, children }) {
 		// Send formData to back-end
 	};
 
-	const fields = fieldsData.map((field, index) => (
-		<LabelledField
-			key={index}
-			type={field.type}
-			labelContent={field.labelContent}
-			inputName={field.inputName}
-			value={formData[field.inputName]}
-			handleInput={handleInput}
-		/>
-	));
-
 	return (
 		<form onSubmit={handleSubmit}>
-			<ul className="section-fields">{fields}</ul>
+			<Fields sectionFields={fieldsData} handleInput={handleInput} />
 			<div className="form-buttons">
 				<Button type="submit" style="emphasis">
 					Submit
