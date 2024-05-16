d6fb6349fc8c11f075b88486f48e9154a9b381d7
Chris <chris79gp@outlook.com>
2024-05-16 13:41:24 +0100

    refactor: udpate class names and html tags in Modal component rename .modal-content to .modal-window replace <div> tag with <header> tag for .modal-header replace "header" selector with #nav selector in NavBar.css to avoid applying #nav styling to non-#nav <header> elements prefix .floating-nav selecting with #nav to ensure specificity is higher than #nav selector when toggling .floating-nav styling

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -43,13 +43,13 @@ export default function Modal({ heading }) {
 				{heading}
 			</Button>
 			<dialog className="modal-overlay" onClick={closeModal} ref={dialogRef} open={isOpen}>
-				<div className="modal-content" onClick={stopPropagation}>
-					<div className="modal-header">
+				<div className="modal-window" onClick={stopPropagation}>
+					<header className="modal-header">
 						<h3>{heading}</h3>
 						<button className="close-modal" onClick={closeModal} title="Close window">
 							<img src="./src/assets/icons/close.svg" alt="Close icon" />
 						</button>
-					</div>
+					</header>
 					<ModalForm closeForm={closeModal} />
 				</div>
 			</dialog>
