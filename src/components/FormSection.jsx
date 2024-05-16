a1093b0179fe19753d0109c91e565640246bb017
Chris <chris79gp@outlook.com>
2024-05-16 22:11:03 +0100

    feat: add FormSection component FormSection returns a <div> with class .form-section with a <h3> and {children} FormSection has props "heading" and "children" "heading" is a string which is inserted as the textContent of the <h3> element {children} are rendered after the <h3> elements abstract styles for .form-section class from Booking.css to FormSection.css

---
+++ src/components/FormSection.jsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+import "../styles/FormSection.css";
+
+export default function FormSection({ heading, children }) {
+	return (
+		<div className="form-section">
+			<h3 className="form-section-heading">{heading}</h3>
+			{children}
+		</div>
+	);
+}
+
+FormSection.propTypes = {
+	heading: PropTypes.string,
+	children: PropTypes.node,
+};
