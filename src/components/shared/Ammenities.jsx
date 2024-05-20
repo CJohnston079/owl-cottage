2b02080f48cb9941503b58c903a98e0139a1c5e0
Chris <chris79gp@outlook.com>
2024-05-20 14:53:58 +0100

    refactor: replace markup for amenities in Amenities with Amenity components

--- src/components/shared/Ammenities.jsx
+++ src/components/shared/Ammenities.jsx
@@ -1,36 +1,14 @@
+import Amenity from "./Amenity";
 import "../../styles/shared/Amenities.css";
 
 export default function Amenities() {
 	return (
 		<div id="amenities">
-			<div className="amenity">
-				<img className="amenity-icon" src="./src/assets/icons/amenities/pet.png" alt="Pet icon" />
-				<span className="amenity-text">Pets allowed</span>
-			</div>
-			<div className="amenity">
-				<img
-					className="amenity-icon"
-					src="./src/assets/icons/amenities/calendar-plus.png"
-					alt="Long-term stay icon"
-				/>
-				<span className="amenity-text">Long-term stays allowed</span>
-			</div>
-			<div className="amenity">
-				<img
-					className="amenity-icon"
-					src="./src/assets/icons/amenities/key.png"
-					alt="Self check-in icon"
-				/>
-				<span className="amenity-text">Self check-in</span>
-			</div>
-			<div className="amenity">
-				<img
-					className="amenity-icon"
-					src="./src/assets/icons/amenities/safe-in.png"
-					alt="Lockbox icon"
-				/>
-				<span className="amenity-text">Lockbox</span>
-			</div>
+			<h3>test</h3>
+			<Amenity icon="pet" content="Pets allowed" />
+			<Amenity icon="calendar-plus" content="Long-term stays allowed" />
+			<Amenity icon="key" content="Self check-in" />
+			<Amenity icon="safe" content="Lockbox" />
 		</div>
 	);
 }
