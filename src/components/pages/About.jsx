14af81bb6ff4b8048f5bb8980a050aacb4751154
Chris <chris79gp@outlook.com>
2024-05-20 14:22:52 +0100

    feat: add amenities to #about

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -40,6 +40,37 @@ export default function About() {
 					</figure>
 				</div>
 			</FlexContainer>
+			<h3>Amenities</h3>
+			<div className="amenities">
+				<div className="amenity">
+					<img className="amenity-icon" src="./src/assets/icons/amenities/pet.png" alt="Pet icon" />
+					<span className="amenity-text">Pets allowed</span>
+				</div>
+				<div className="amenity">
+					<img
+						className="amenity-icon"
+						src="./src/assets/icons/amenities/calendar-plus.png"
+						alt="Long-term stay icon"
+					/>
+					<span className="amenity-text">Long-term stays allowed</span>
+				</div>
+				<div className="amenity">
+					<img
+						className="amenity-icon"
+						src="./src/assets/icons/amenities/key.png"
+						alt="Self check-in icon"
+					/>
+					<span className="amenity-text">Self check-in</span>
+				</div>
+				<div className="amenity">
+					<img
+						className="amenity-icon"
+						src="./src/assets/icons/amenities/safe-in.png"
+						alt="Lockbox icon"
+					/>
+					<span className="amenity-text">Lockbox</span>
+				</div>
+			</div>
 		</Section>
 	);
 }
