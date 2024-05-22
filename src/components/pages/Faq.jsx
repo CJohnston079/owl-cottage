c9e88f0c18c4f700023864044c8aa982e162ff76
Chris <chris79gp@outlook.com>
2024-05-22 18:04:30 +0100

    refactor: replace {content} prop of AccordionContainer with FaqAnswer component in Faq.jsx

--- src/components/pages/Faq.jsx
+++ src/components/pages/Faq.jsx
@@ -1,11 +1,13 @@
 import AccordionContainer from "../shared/AccordionContainer";
+import FaqAnswer from "../shared/FaqAnswer";
 import Section from "../shared/Section";
 import faqData from "../../data/faq.json";
 
 export default function Faq() {
 	const faqElements = faqData.map(data => {
-		return { heading: data.q, content: data.a };
+		return { heading: data.q, content: <FaqAnswer answer={data.a} /> };
 	});
+
 	return (
 		<Section sectionId="faq" heading="Frequently asked questions">
 			<AccordionContainer items={faqElements}></AccordionContainer>
