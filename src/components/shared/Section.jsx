cb0f6f5c5cdcc2d198f8e2de16b7d6166ee89ca9
Chris <chris79gp@outlook.com>
2024-05-22 17:04:00 +0100

    feat: add {heading} prop to Section component {heading} defaults to {sectionId} if none provided refactor sectionHeading to replace "#" character instead of slicing away the first character to ensure it correctly formats cases where {heading} prop is provided

--- src/components/shared/Section.jsx
+++ src/components/shared/Section.jsx
@@ -1,8 +1,8 @@
 import PropTypes from "prop-types";
 import "../../styles/shared/Section.css";
 
-export default function Section({ sectionId, children }) {
-	const sectionHeading = sectionId.charAt(0).toUpperCase() + sectionId.slice(1);
+export default function Section({ sectionId, heading = sectionId, children }) {
+	const sectionHeading = heading.replace("#", "").charAt(0).toUpperCase() + heading.slice(1);
 
 	return (
 		<section id={sectionId}>
@@ -14,5 +14,6 @@ export default function Section({ sectionId, children }) {
 
 Section.propTypes = {
 	sectionId: PropTypes.string,
+	heading: PropTypes.string,
 	children: PropTypes.node,
 };
