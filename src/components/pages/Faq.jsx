e964c3f156d29c8b4c53167cc43d24abb64f68eb
Chris <chris79gp@outlook.com>
2024-05-22 16:59:41 +0100

    feat: render accordion items from faq.json

--- src/components/pages/Faq.jsx
+++ src/components/pages/Faq.jsx
@@ -1,10 +1,14 @@
 import AccordionContainer from "../shared/AccordionContainer";
 import Section from "../shared/Section";
+import faqData from "../../data/faq.json";
 
 export default function Faq() {
+	const faqElements = faqData.map(data => {
+		return { heading: data.q, content: data.a };
+	});
 	return (
 		<Section sectionId="faq">
-			<AccordionContainer items={[{ heading: "question", content: "answer" }]}></AccordionContainer>
+			<AccordionContainer items={faqElements}></AccordionContainer>
 		</Section>
 	);
 }
