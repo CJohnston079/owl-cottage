08bbdf7085746c29d7fbf32d5de8863d493232df
Chris <chris79gp@outlook.com>
2024-05-24 14:58:34 +0100

    fix: dynamically import icons in Amenity.jsx to resolve issue where images are not displaying on deployment site

--- src/components/shared/Amenity.jsx
+++ src/components/shared/Amenity.jsx
@@ -1,14 +1,22 @@
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import "../../styles/shared/Amenity.css";
 
 export default function Amenity({ icon, content }) {
+	const [iconPath, setIconPath] = useState(null);
+
+	useEffect(() => {
+		const fetchIcon = async () => {
+			const amenityIcon = await import(`../../assets/icons/amenities/${icon}.png`);
+			setIconPath(amenityIcon.default);
+		};
+
+		fetchIcon();
+	}, [icon]);
+
 	return (
 		<div className="amenity">
-			<img
-				className="amenity-icon"
-				src={`./src/assets/icons/amenities/${icon}.png`}
-				alt={`${icon} icon`}
-			/>
+			{iconPath && <img className="amenity-icon" src={iconPath} alt={`${icon} icon`} />}
 			<span className="amenity-text">{content}</span>
 		</div>
 	);
