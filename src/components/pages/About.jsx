bf391c90d5394deabb6dfa5029251bb6fdb2273f
Chris <chris79gp@outlook.com>
2024-05-24 22:21:41 +0100

    feat: add "Cottage" subsection to About and move existing elements to "Amenities" subsection add .bento-grid container for images replace icons for featured amenities update descriptions of features amenities add "Take a tour" button underneath cottage description

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,50 +1,63 @@
 import Amenities from "../shared/Amenities";
-// import Button from "../shared/Button";
+import Button from "../shared/Button";
 import Modal from "../shared/Modal";
-import FlexContainer from "../shared/FlexContainer";
 import Section from "../shared/Section";
 import "../../styles/pages/About.css";
 
 export default function About() {
 	return (
 		<Section sectionId="about">
-			<FlexContainer>
-				<div className="feature">
+			<div style={{ marginBottom: "4rem" }} className="grid">
+				<div className="">
+					<h3>The Cottage</h3>
 					<p>
 						A traditional stone cottage built in 1868, Owl Cottage features a large, sociable
 						kitchen/dining/living area with wood burner, master bedroom with king size bed, second
 						bedroom with bunk beds, shower room, enclosed back garden, off-street parking and
 						stunning views over the forest and mountains opposite.
 					</p>
+					<br />
 					<p>
 						Dogs are very welcome here and can enjoy the enclosed garden and dog-friendly furniture
 						while you relax in the spacious open-plan living area with log-burner. Amazing views of
 						mountains & countryside - you may even see deer in the garden!
 					</p>
-					{/* <Button style="emphasis">Take a tour of Owl ➞</Button> */}
-					<Modal buttonText="Full list of amenities" heading="Amenities" showCloseButton={true}>
-						{() => <Amenities id="wee" />}
-					</Modal>
+					<br />
+					<Button style="emphasis">Take a tour of Owl ➞</Button>
 				</div>
-				<div className="img-grid">
-					<figure className="icon-with-caption">
-						<img src="./src/assets/icons/amenities/bed.png" alt="" />
-						<figcaption>2 bedrooms</figcaption>
-					</figure>
-					<figure className="icon-with-caption">
-						<img src="./src/assets/icons/amenities/pet.png" alt="" />
-						<figcaption>Pet friendly</figcaption>
-					</figure>
-					<figure className="icon-with-caption">
-						<img src="./src/assets/icons/amenities/parking.png" alt="" />
-						<figcaption>Driveway</figcaption>
-					</figure>
-					<figure className="icon-with-caption">
-						<img src="./src/assets/icons/amenities/landscape.png" alt="" />
-						<figcaption>Mountain view</figcaption>
-					</figure>
+				<div className="bento-grid">
+					<div className="bento-column">
+						<img className="bento-img" src="./src/assets/images/owl-kitchen.webp" alt="s" />
+						<img className="bento-img" src="./src/assets/images/owl-bedroom.webp" alt="s" />
+					</div>
+					<div className="bento-column">
+						<img className="bento-img" src="./src/assets/images/owl-signpost.webp" alt="s" />
+						<img className="bento-img" src="./src/assets/images/owl-furniture.webp" alt="s" />
+					</div>
+					<div className="bento-column">
+						<img className="bento-img" src="./src/assets/images/owl-garden.webp" alt="s" />
+						<img className="bento-img" src="./src/assets/images/owl-coffee.webp" alt="s" />
+					</div>
 				</div>
-			</FlexContainer>
+			</div>
+			<h3>Amenities</h3>
+			<div style={{ display: "flex", gap: "1rem", marginBottom: "2rem" }}>
+				<figure className="icon-with-caption">
+					<img src="./src/assets/bed.png" alt="" />
+					<figcaption>2 bedrooms, sleeps up to four guests</figcaption>
+				</figure>
+				<figure className="icon-with-caption">
+					<img src="./src/assets/mountains.png" alt="" />
+					<figcaption>Amazing views of mountains & countryside</figcaption>
+				</figure>
+				<figure className="icon-with-caption">
+					<img src="./src/assets/dog.png" alt="" />
+					<figcaption>Dog-friendly, includng furniture and enclosed garden</figcaption>
+				</figure>
+			</div>
+			<Modal buttonText="Full list of amenities ➞" heading="Amenities" showCloseButton={true}>
+				{() => <Amenities id="wee" />}
+			</Modal>
 		</Section>
 	);
 }
