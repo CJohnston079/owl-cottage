fa0c8ddc1e141897efa181e19b59c4d2013ae7db
Chris <chris79gp@outlook.com>
2024-05-25 21:53:35 +0100

    refactor: abstract markup and styling for #amenities section to mew Amenities component

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,6 +1,5 @@
-import AmenitiesList from "../shared/AmenitiesList";
+import Amenities from "../shared/Amenities";
 import Introduction from "../shared/Introduction";
-import Modal from "../shared/Modal";
 import Section from "../shared/Section";
 import "../../styles/pages/About.css";
 
@@ -8,24 +7,7 @@ export default function About() {
 	return (
 		<Section sectionId="about">
 			<Introduction />
-			<h3>Amenities</h3>
-			<div style={{ display: "flex", gap: "1rem", marginBottom: "2rem" }}>
-				<figure className="icon-with-caption">
-					<img src="./src/assets/bed.png" alt="" />
-					<figcaption>2 bedrooms, sleeps up to four guests</figcaption>
-				</figure>
-				<figure className="icon-with-caption">
-					<img src="./src/assets/mountains.png" alt="" />
-					<figcaption>Amazing views of mountains & countryside</figcaption>
-				</figure>
-				<figure className="icon-with-caption">
-					<img src="./src/assets/dog.png" alt="" />
-					<figcaption>Dog-friendly, includng furniture and enclosed garden</figcaption>
-				</figure>
-			</div>
-			<Modal buttonText="Full list of amenities ➞" heading="Amenities" showCloseButton={true}>
-				{() => <AmenitiesList id="wee" />}
-			</Modal>
+			<Amenities />
 		</Section>
 	);
 }
---
+++ src/components/shared/Amenities.jsx
@@ -0,0 +1,28 @@
+import AmenitiesList from "./AmenitiesList";
+import Modal from "../shared/Modal";
+import "../../styles/shared/Amenities.css";
+
+export default function Amenities() {
+	return (
+		<div id="amenities">
+			<h3>Amenities</h3>
+			<div style={{ display: "flex", gap: "1rem", marginBottom: "2rem" }}>
+				<figure className="icon-with-caption">
+					<img src="./src/assets/bed.png" alt="" />
+					<figcaption>2 bedrooms, sleeps up to four guests</figcaption>
+				</figure>
+				<figure className="icon-with-caption">
+					<img src="./src/assets/mountains.png" alt="" />
+					<figcaption>Amazing views of mountains & countryside</figcaption>
+				</figure>
+				<figure className="icon-with-caption">
+					<img src="./src/assets/dog.png" alt="" />
+					<figcaption>Dog-friendly, includng furniture and enclosed garden</figcaption>
+				</figure>
+			</div>
+			<Modal buttonText="Full list of amenities ➞" heading="Amenities" showCloseButton={true}>
+				{() => <AmenitiesList id="wee" />}
+			</Modal>
+		</div>
+	);
+}
