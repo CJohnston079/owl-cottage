05fca9a527127498a0b25becff524a43eac8650a
Chris <chris79gp@outlook.com>
2024-05-25 22:40:29 +0100

    feat: add styling for Amenities components

--- src/components/shared/Amenities.jsx
+++ src/components/shared/Amenities.jsx
@@ -5,34 +5,35 @@ import "../../styles/shared/Amenities.css";
 export default function Amenities() {
 	return (
 		<div id="amenities">
-			<h3>Amenities</h3>
-			<div style={{ display: "flex", gap: "1rem", marginBottom: "2rem" }}>
-				<figure className="icon-with-caption">
-					<img src="./src/assets/bed.png" alt="" />
-					<figcaption>
-						<h4>2 bedrooms, sleeps up to four guests</h4>
-						<p>
+			<h2>Amenities</h2>
+			<br />
+			<div className="amenities-container">
+				<figure className="ft-amenity">
+					<img className="ft-amenity-icon" src="./src/assets/bed.png" alt="" />
+					<figcaption className="ft-amenity-text-wrapper">
+						<h4 className="ft-amenity-heading">Two bedrooms, sleeps up to four guests</h4>
+						<p className="ft-amenity-description">
 							The cottage is furnished with one king-sized bed and one bunk bed. Please note that
 							the top bunk bed is for those aged six and over and for a maximum of 60kg.
 						</p>
 					</figcaption>
 				</figure>
-				<figure className="icon-with-caption">
-					<img src="./src/assets/mountains.png" alt="" />
-					<figcaption>
-						<h4>Amazing views of mountains & countryside</h4>
-						<p>
+				<figure className="ft-amenity">
+					<img className="ft-amenity-icon" src="./src/assets/mountains.png" alt="" />
+					<figcaption className="ft-amenity-text-wrapper">
+						<h4 className="ft-amenity-heading">Amazing views of mountains & countryside</h4>
+						<p className="ft-amenity-description">
 							The view from the front looks out over forest to the Corbett <i>Meall Dubh</i> (
 							<i>Black Hill</i>), the highest point of the moors between Glen Garry and Glen
 							Moriston.
 						</p>
 					</figcaption>
 				</figure>
-				<figure className="icon-with-caption">
-					<img src="./src/assets/dog.png" alt="" />
-					<figcaption>
-						<h4>Dog-friendly, includng furniture and enclosed garden</h4>
-						<p>
+				<figure className="ft-amenity">
+					<img className="ft-amenity-icon" src="./src/assets/dog.png" alt="" />
+					<figcaption className="ft-amenity-text-wrapper">
+						<h4 className="ft-amenity-heading">Dog-friendly furniture and enclosed garden</h4>
+						<p className="ft-amenity-description">
 							Dogs are very welcome here and can enjoy the enclosed garden and dog-friendly
 							furniture while you relax in the spacious open-plan living area with log-burner.
 						</p>
