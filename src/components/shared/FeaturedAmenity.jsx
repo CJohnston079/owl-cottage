122d6b2ec2114c409ab9498ca9123e6a5e8d5a71
Chris <chris79gp@outlook.com>
2024-05-27 08:51:17 +0100

    feat: add FeaturedAmenity component returns a figure with a heading and caption has props iconSrc, heading and content

---
+++ src/components/shared/FeaturedAmenity.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+
+export default function FeaturedAmenity({ iconSrc, heading, content }) {
+	return (
+		<figure className="ft-amenity">
+			<img className="ft-amenity-icon" src={iconSrc} alt="" />
+			<hr />
+			<figcaption className="ft-amenity-text-wrapper">
+				<h4 className="ft-amenity-heading">{heading}</h4>
+				<p className="ft-amenity-description">{content}</p>
+			</figcaption>
+		</figure>
+	);
+}
+
+FeaturedAmenity.propTypes = {
+	iconSrc: PropTypes.string,
+	heading: PropTypes.string,
+	content: PropTypes.string,
+};
