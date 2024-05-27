6d5f116fef07f18c10459891bc9593b0f0cfb1f1
Chris <chris79gp@outlook.com>
2024-05-27 08:54:00 +0100

    refactor: replace .ft-amenity elements in Amenities.jsx with FeaturedAmenity component

--- src/components/pages/Amenities.jsx
+++ src/components/pages/Amenities.jsx
@@ -1,3 +1,4 @@
+import FeaturedAmenity from "../shared/FeaturedAmenity";
 import AmenitiesList from "../shared/AmenitiesList";
 import Modal from "../shared/Modal";
 import Section from "../shared/Section";
@@ -11,40 +12,21 @@ export default function Amenities() {
 	return (
 		<Section sectionId="amenities">
 			<div className="amenities-container">
-				<figure className="ft-amenity">
-					<img className="ft-amenity-icon" src={bedIcon} alt="" />
-					<hr />
-					<figcaption className="ft-amenity-text-wrapper">
-						<h4 className="ft-amenity-heading">Two bedrooms, sleeps up to four guests</h4>
-						<p className="ft-amenity-description">
-							The cottage is furnished with one king-sized bed and one bunk bed. Please note that
-							the top bunk bed is for those aged six and over and for a maximum of 60kg.
-						</p>
-					</figcaption>
-				</figure>
-				<figure className="ft-amenity">
-					<img className="ft-amenity-icon" src={mountainsIcon} alt="" />
-					<hr />
-					<figcaption className="ft-amenity-text-wrapper">
-						<h4 className="ft-amenity-heading">Amazing views of mountains & countryside</h4>
-						<p className="ft-amenity-description">
-							The view from the front looks out over forest to the Corbett <i>Meall Dubh</i> (
-							<i>Black Hill</i>), the highest point of the moors between Glen Garry and Glen
-							Moriston.
-						</p>
-					</figcaption>
-				</figure>
-				<figure className="ft-amenity">
-					<img className="ft-amenity-icon" src={dogIcon} alt="" />
-					<hr />
-					<figcaption className="ft-amenity-text-wrapper">
-						<h4 className="ft-amenity-heading">Dog-friendly furniture and enclosed garden</h4>
-						<p className="ft-amenity-description">
-							Dogs are very welcome here and can enjoy the enclosed garden and dog-friendly
-							furniture while you relax in the spacious open-plan living area with log-burner.
-						</p>
-					</figcaption>
-				</figure>
+				<FeaturedAmenity
+					iconSrc={bedIcon}
+					heading="Two bedrooms, sleeps up to four guests"
+					content="The cottage is furnished with one king-sized bed and one bunk bed. Please note that the top bunk bed is for those aged six and over and for a maximum of 60kg."
+				/>
+				<FeaturedAmenity
+					iconSrc={mountainsIcon}
+					heading="Amazing views of mountains & countryside"
+					content="The view from the front looks out over forest to the Corbett Meall Dubh (Black Hill), the highest point of the moors between Glen Garry and Glen Moriston."
+				/>
+				<FeaturedAmenity
+					iconSrc={dogIcon}
+					heading="Dog-friendly furniture and enclosed garden"
+					content="Dogs are very welcome here and can enjoy the enclosed garden and dog-friendly furniture while you relax in the spacious open-plan living area with log-burner."
+				/>
 			</div>
 			<Modal buttonText="Full list of amenities ➞" heading="Amenities" showCloseButton={true}>
 				{() => <AmenitiesList id="wee" />}
