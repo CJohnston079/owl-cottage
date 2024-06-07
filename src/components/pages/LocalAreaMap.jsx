19fba4c1de9ac12e539bff7f92cc7f7f743e7c9d
Chris <chris79gp@outlook.com>
2024-06-07 16:01:03 +0100

    feat: update styling of LocalAreaMap

--- src/components/pages/LocalAreaMap.jsx
+++ src/components/pages/LocalAreaMap.jsx
@@ -1,22 +1,19 @@
-import FlexContainer from "../shared/FlexContainer";
 import Section from "../shared/Section";
 import "../../styles/pages/LocalAreaMap.css";
 
-import lochOich from "../../assets/images/loch-oich.webp";
-import fortAugustus from "../../assets/images/fort-augustus.webp";
 import lochNess from "../../assets/images/loch-ness.jpeg";
 
 export default function LocalAreaMap() {
 	return (
-		<Section sectionId="local-area-map">
-			<iframe
-				className="map"
-				src="https://snazzymaps.com/embed/611220"
-				width="100%"
-				height="600px"
-				style={{ border: "none" }}
-			></iframe>
-			<FlexContainer>
+		<Section sectionId="points-of-interest">
+			<div className="grid-container">
+				<div className="poi-container">
+					<button className="poi">Loch Oich</button>
+					<button className="poi">Loch Ness</button>
+					<button className="poi">Fort Augustus</button>
+					<button className="poi">Fort William</button>
+					<button className="poi">Great Glen</button>
+				</div>
 				<figure className="img-with-caption">
 					<img src={lochNess} alt="Urquhart Castle, Loch Ness" />
 					<figcaption>
@@ -27,28 +24,14 @@ export default function LocalAreaMap() {
 						</p>
 					</figcaption>
 				</figure>
-				<figure className="img-with-caption">
-					<img src={fortAugustus} alt="Fort Augustus, Inverness-shire" />
-					<figcaption>
-						<h3>Fort Augustus</h3>
-						<p>
-							Nearby Fort Augustus has a good range of local shops and attractions, cycle hire and
-							boat trips on Loch Ness.
-						</p>
-					</figcaption>
-				</figure>
-				<figure className="img-with-caption">
-					<img src={lochOich} alt="Loch Oich shore" />
-					<figcaption>
-						<h3>Loch Oich</h3>
-						<p>
-							Beautiful Loch Oich is just a five minute drive away - join the Great Glen Way here
-							and enjoy some beautiful walks, an excellent cycle route, or even a wild swim for the
-							brave.
-						</p>
-					</figcaption>
-				</figure>
-			</FlexContainer>
+				<iframe
+					className="map"
+					src="https://snazzymaps.com/embed/611220"
+					width="100%"
+					height="600px"
+					style={{ border: "none" }}
+				></iframe>
+			</div>
 		</Section>
 	);
 }
