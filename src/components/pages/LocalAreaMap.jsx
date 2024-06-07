00752d07228069cb89fb2429c42a2823357f8ebe
Chris <chris79gp@outlook.com>
2024-06-07 19:39:01 +0100

    feat: update styling for #points-of-interest

--- src/components/pages/LocalAreaMap.jsx
+++ src/components/pages/LocalAreaMap.jsx
@@ -1,26 +1,21 @@
 import Section from "../shared/Section";
 import "../../styles/pages/LocalAreaMap.css";
 
-import lochNess from "../../assets/images/loch-ness.jpeg";
+import lochOich from "../../assets/images/loch-oich.webp";
 
 export default function LocalAreaMap() {
 	return (
 		<Section sectionId="points-of-interest">
 			<div className="grid-container">
-				<div className="poi-container">
-					<button className="poi">Loch Oich</button>
-					<button className="poi">Loch Ness</button>
-					<button className="poi">Fort Augustus</button>
-					<button className="poi">Fort William</button>
-					<button className="poi">Great Glen</button>
-				</div>
 				<figure className="img-with-caption">
-					<img src={lochNess} alt="Urquhart Castle, Loch Ness" />
+					<img src={lochOich} alt="Urquhart Castle, Loch Ness" />
 					<figcaption>
-						<h3>Loch Ness</h3>
+						<h3>Bridge of Oich</h3>
 						<p>
-							The world-famous Loch Ness is just three miles away. Pictured here is 13th Century
-							Castle Urquhart.
+							Loch Oich is just five minutes away. Not as well known as Loch Ness but a real hidden
+							treasure. The Great Glen Way runs along the loch shore and its flat, level path here
+							makes it highly accessible for walking and cycling. Very dog friendly with no nearby
+							roads or livestock. Park at Bridge of Oich to access the loch.
 						</p>
 					</figcaption>
 				</figure>
@@ -31,6 +26,14 @@ export default function LocalAreaMap() {
 					height="600px"
 					style={{ border: "none" }}
 				></iframe>
+			</div>
+			<div className="poi-container">
+				<button className="poi active">Bridge of Oich</button>
+				<button className="poi">Fort Augustus</button>
+				<button className="poi">Loch Ness</button>
+				<button className="poi">Alt na Calliche Trail</button>
+				<button className="poi">Falls of Foyers</button>
+				<button className="poi">Isle of Skye</button>
 			</div>
 		</Section>
 	);
