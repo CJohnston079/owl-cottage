5fedd8af5b68d6a9a1731aae37fd067e47be5bc6
Chris <chris79gp@outlook.com>
2024-06-09 21:33:31 +0100

    refactor: replace jsx for <p> elements and Bento with ContentWithBento component remove redundant styles from Location.css

--- src/components/pages/Location.jsx
+++ src/components/pages/Location.jsx
@@ -1,4 +1,4 @@
-import Bento from "../shared/Bento";
+import ContentWithBento from "../shared/ContentWithBento";
 import Section from "../shared/Section";
 import "../../styles/pages/Location.css";
 
@@ -21,31 +21,28 @@ export default function Location() {
 
 	return (
 		<Section sectionId="location">
-			<div className="grid-container">
-				<div className="p-container">
-					<p>
-						Loch Oich is a real hidden treasure, and just a five mintue drive from the cottage. The
-						Great Glen Way runs along the loch shore and its flat, level path here makes it highly
-						accessible for walking and cycling. Very dog friendly with no nearby roads or livestock,
-						and easy parking at Bridge of Oich to access the loch.
-					</p>
-					<p>
-						The Caledonian Canal from Bridge of Oich to Fort Augustus is another flat, accessible
-						walk, suitable for walkers, cyclists and dogs. Follow the quiet footpath through
-						beautiful forestry to reach the famous Fort Augustus Locks and the start of Loch Ness.
-					</p>
-					<p>
-						Here you can hire bicycles, have a boat trip on the loch, or for the more adventurous,
-						several companies offer kayaking, canoeing and white water adventures.
-					</p>
-					<p>
-						Many more wonderful walks are within easy reach - at all levels from short, flat paths
-						to challenging Munros. And the ultimate challenge of Ben Nevis is just 40 minutes away.
-					</p>
-					<p>The Isle of Skye is an hour&apos;s drive west, and Inverness is an hour north.</p>
-				</div>
-				<Bento images={images} />
-			</div>
+			<ContentWithBento bentoImages={images}>
+				<p>
+					Loch Oich is a real hidden treasure, and just a five mintue drive from the cottage. The
+					Great Glen Way runs along the loch shore and its flat, level path here makes it highly
+					accessible for walking and cycling. Very dog friendly with no nearby roads or livestock,
+					and easy parking at Bridge of Oich to access the loch.
+				</p>
+				<p>
+					The Caledonian Canal from Bridge of Oich to Fort Augustus is another flat, accessible
+					walk, suitable for walkers, cyclists and dogs. Follow the quiet footpath through beautiful
+					forestry to reach the famous Fort Augustus Locks and the start of Loch Ness.
+				</p>
+				<p>
+					Here you can hire bicycles, have a boat trip on the loch, or for the more adventurous,
+					several companies offer kayaking, canoeing and white water adventures.
+				</p>
+				<p>
+					Many more wonderful walks are within easy reach - at all levels from short, flat paths to
+					challenging Munros. And the ultimate challenge of Ben Nevis is just 40 minutes away.
+				</p>
+				<p>The Isle of Skye is an hour&apos;s drive west, and Inverness is an hour north.</p>
+			</ContentWithBento>
 		</Section>
 	);
 }
