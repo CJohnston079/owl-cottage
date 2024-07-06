c6197a5bc07bc269ce893623568b7abca6da1e13
Chris <chris79gp@outlook.com>
2024-07-06 22:10:10 +0100

    feat: conditionally render different descriptions for points of interest in `LocalAreaMap.jsx`

--- src/components/sections/LocalAreaMap.jsx
+++ src/components/sections/LocalAreaMap.jsx
@@ -11,12 +11,55 @@ import fortAugustus from "../../assets/images/locations/poi-fort-augustus.webp";
 
 export default function LocalAreaMap() {
 	const pointsOfInterest = [
-		{ name: "Bridge of Oich", src: bridgeOfOich, alt: "Bridge of Oich" },
-		{ name: "Fort Augustus", src: fortAugustus, alt: "Fort Augustus" },
-		{ name: "Loch Ness", src: lochNess, alt: "Loch Ness" },
-		{ name: "Alt na Calliche Trail", src: invergarryWoods, alt: "Invergarry Woods" },
-		{ name: "Falls of Foyers", src: fallOfFoyers, alt: "Fall of Foyers" },
-		{ name: "Isle of Skye", src: isleOfSkye, alt: "Portree, Isle of Skye" },
+		{
+			name: "Bridge of Oich",
+			src: bridgeOfOich,
+			alt: "Bridge of Oich",
+			description: [
+				"Loch Oich is just five minutes away. Not as well known as Loch Ness but a real hidden treasure. The Great Glen Way runs along the loch shore and its flat, level path here makes it highly accessible for walking and cycling. Very dog friendly with no nearby roads or livestock. Park at Bridge of Oich to access the loch.",
+				"The Loch Oich circuit is a great walk that can be tackled in one or broken down into sections - the flat wide cycle path on the east of the loch is ideal for those seeking a more accessible walk.",
+			],
+		},
+		{
+			name: "Fort Augustus",
+			src: fortAugustus,
+			alt: "Fort Augustus",
+			description: [
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices in iaculis nunc sed augue lacus. Metus aliquam eleifend mi in nulla posuere sollicitudin.",
+			],
+		},
+		{
+			name: "Loch Ness",
+			src: lochNess,
+			alt: "Loch Ness",
+			description: [
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices in iaculis nunc sed augue lacus. Metus aliquam eleifend mi in nulla posuere sollicitudin.",
+			],
+		},
+		{
+			name: "Alt na Calliche Trail",
+			src: invergarryWoods,
+			alt: "Invergarry Woods",
+			description: [
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices in iaculis nunc sed augue lacus. Metus aliquam eleifend mi in nulla posuere sollicitudin.",
+			],
+		},
+		{
+			name: "Falls of Foyers",
+			src: fallOfFoyers,
+			alt: "Fall of Foyers",
+			description: [
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices in iaculis nunc sed augue lacus. Metus aliquam eleifend mi in nulla posuere sollicitudin.",
+			],
+		},
+		{
+			name: "Isle of Skye",
+			src: isleOfSkye,
+			alt: "Portree, Isle of Skye",
+			description: [
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices in iaculis nunc sed augue lacus. Metus aliquam eleifend mi in nulla posuere sollicitudin.",
+			],
+		},
 	];
 
 	const [selectedPOI, setSelectedPOI] = useState(pointsOfInterest[0]);
@@ -25,6 +68,8 @@ export default function LocalAreaMap() {
 		setSelectedPOI(poi);
 	};
 
+	const paragraphs = selectedPOI.description.map((description, i) => <p key={i}>{description}</p>);
+
 	return (
 		<Section sectionId="points-of-interest">
 			<div className="map-container">
@@ -32,11 +77,7 @@ export default function LocalAreaMap() {
 					<img src={selectedPOI.src} alt={selectedPOI.alt} />
 					<figcaption>
 						<h3>{selectedPOI.name}</h3>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-							incididunt ut labore et dolore magna aliqua. Ultrices in iaculis nunc sed augue lacus.
-							Metus aliquam eleifend mi in nulla posuere sollicitudin.
-						</p>
+						{paragraphs}
 					</figcaption>
 				</figure>
 				<iframe
