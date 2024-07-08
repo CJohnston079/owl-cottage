cff24980878a05ee567fc0875de5a1854881d606
Chris <chris79gp@outlook.com>
2024-07-08 18:39:57 +0100

    feat: add Licence Number to `Footer`

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -3,7 +3,7 @@ import "../../styles/sections/Footer.css";
 export default function Footer() {
 	return (
 		<footer>
-			<p>Copyright © 2024 Owl Cottage. All rights reserved.</p>
+			<p>Copyright © 2024 Owl Cottage. All rights reserved. Licence number: HI-40029-F.</p>
 		</footer>
 	);
 }
