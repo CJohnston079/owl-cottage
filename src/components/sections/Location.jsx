06a88ad729a0b83682c7d656c36b0a8f0d555106
Chris <chris79gp@outlook.com>
2024-07-14 21:21:17 +0100

    feat: update copy for `Location`

--- src/components/sections/Location.jsx
+++ src/components/sections/Location.jsx
@@ -23,25 +23,26 @@ export default function Location() {
 		<Section sectionId="location">
 			<ContentWithBento bentoImages={images}>
 				<p>
-					Loch Oich is a real hidden treasure, and just a five mintue drive from the cottage. The
-					Great Glen Way runs along the loch shore and its flat, level path here makes it highly
-					accessible for walking and cycling. Very dog friendly with no nearby roads or livestock,
-					and easy parking at Bridge of Oich to access the loch.
+					<b>
+						Set in the heart of the Great Glen, Owl Cottage is your perfect base to explore the
+						highlands.
+					</b>
 				</p>
 				<p>
-					The Caledonian Canal from Bridge of Oich to Fort Augustus is another flat, accessible
-					walk, suitable for walkers, cyclists and dogs. Follow the quiet footpath through beautiful
-					forestry to reach the famous Fort Augustus Locks and the start of Loch Ness.
+					The Great Glen (<i>An Gleann Mòr</i>) cuts through the centre of the Scottish Highlands
+					from Fort William in the south to Inverness in the north. It is an area of extraordinary
+					natural beauty, filled with lochs and rivers, forests and mountains, and the famous
+					Caledonian Canal.
 				</p>
 				<p>
-					Here you can hire bicycles, have a boat trip on the loch, or for the more adventurous,
-					several companies offer kayaking, canoeing and white water adventures.
+					Owl Cottage occupies an ideal position right in the middle of the Glen, just south of the
+					charming village of Fort Augustus and the world-famous Loch Ness. Fort William, Inverness
+					and the Isle of Skye can all be reached in around an hour.
 				</p>
 				<p>
-					Many more wonderful walks are within easy reach - at all levels from short, flat paths to
+					Many wonderful walks are within easy reach - at all levels from short, flat paths to
 					challenging Munros. And the ultimate challenge of Ben Nevis is just 40 minutes away.
 				</p>
-				<p>The Isle of Skye is an hour&apos;s drive west, and Inverness is an hour north.</p>
 			</ContentWithBento>
 		</Section>
 	);
