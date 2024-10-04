0b847916c6ff49fac7246fb7dac3f99f9861c1e0
Chris <chris79gp@outlook.com>
2024-10-04 11:13:31 +0100

    Feat: replace booking form with tariff list

--- src/components/sections/Booking.jsx
+++ src/components/sections/Booking.jsx
@@ -1,61 +1,26 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import Button from "../shared/Button";
-import Fields from "../shared/Fields";
-// import Form from "./Form";
-import FormSection from "../shared/FormSection";
+import Card from "../shared/Card";
 import Section from "../shared/Section";
-import formFields from "../../data/formFields.json";
+import Tariff from "../shared/Tariff";
 import "../../styles/sections/Booking.css";
 
 export default function Booking() {
-	const fields = formFields.booking;
-
-	const [formData, setFormData] = useState(() =>
-		Object.values(fields).reduce((data, fieldSection) => {
-			return fieldSection.reduce((subData, field) => ({ ...subData, [field.inputName]: "" }), data);
-		}, {})
-	);
-
-	const handleInput = e => {
-		const { name, value } = e.target;
-		setFormData({ ...formData, [name]: value });
-	};
-
-	const handleSubmit = e => {
-		e.preventDefault();
-		console.log("Booking submitted:", formData);
-		// Send form data to back-end
-		// Reset form fields
-	};
-
 	return (
 		<Section sectionId={"booking"}>
-			<form onSubmit={handleSubmit}>
-				<FormSection heading="Make a booking enquiry">
-					<Fields sectionFields={fields.dates} handleInput={handleInput} />
-					<Fields sectionFields={fields.guestDetails} handleInput={handleInput} />
-					<ul className="section-fields">
-						<li className="field">
-							<label htmlFor="message" className="field-label">
-								Message:
-							</label>
-							<textarea name="message" id="message" className="field-input"></textarea>
-						</li>
-					</ul>
+			<div className="booking-main">
+				<Card>
+					<h3>Tariffs</h3>
+					<Tariff />
 					<Link to="/terms-and-conditions">
 						<Button style="normal">Terms and conditions</Button>
 					</Link>
-					<Button type="submit" style="emphasis">
-						Submit booking request
-					</Button>
-					<hr />
 					<p>
 						For all other enquiries, please email <span>jane@owlcottages.co.uk</span>.
 					</p>
-				</FormSection>
-			</form>
+				</Card>
+			</div>
 		</Section>
 	);
 }
---
+++ src/components/shared/Tariff.jsx
@@ -0,0 +1,110 @@
+import "../../styles/shared/Tariff.css";
+
+export default function Tariff() {
+	return (
+		<>
+			<table>
+				<caption>2024</caption>
+				<thead>
+					<tr>
+						<th>Season</th>
+						<th>
+							Price per night <span className="th-subheading">up to 6 nights</span>
+						</th>
+						<th>
+							Price per night <span className="th-subheading">7+ nights</span>
+						</th>
+						<th>Dates</th>
+					</tr>
+				</thead>
+				<tr>
+					<th>High Season</th>
+					<td>£200</td>
+					<td>£160</td>
+					<td>1 Jul to 31 Aug</td>
+				</tr>
+				<tr>
+					<th>Mid Season</th>
+					<td>£170</td>
+					<td>£136</td>
+					<td>1 May to 30 Jun; 1 to 30 Sep</td>
+				</tr>
+				<tr>
+					<th>Low Season</th>
+					<td>£140</td>
+					<td>£112</td>
+					<td>15 to 30 Apr; 1 to 31 Oct</td>
+				</tr>
+				<tr>
+					<th>Winter Season</th>
+					<td>£100</td>
+					<td>£80</td>
+					<td>2 Jan to 28 Mar; 1 Nov to 22 Dec</td>
+				</tr>
+				<tr>
+					<th>Easter</th>
+					<td>£185</td>
+					<td>£148</td>
+					<td>29 Mar to 14 Apr</td>
+				</tr>
+				<tr>
+					<th>Christmas</th>
+					<td>£185</td>
+					<td>£148</td>
+					<td>23 Dec to 5 Jan</td>
+				</tr>
+			</table>
+			<table>
+				<caption>2025</caption>
+				<thead>
+					<tr>
+						<th>Season</th>
+						<th>
+							Price per night <span className="th-subheading">up to 6 nights</span>
+						</th>
+						<th>
+							Price per night <span className="th-subheading">7+ nights</span>
+						</th>
+						<th>Dates</th>
+					</tr>
+				</thead>
+				<tr>
+					<th>High Season</th>
+					<td>£220</td>
+					<td>£176</td>
+					<td>1 Jul to 31 Aug</td>
+				</tr>
+				<tr>
+					<th>Mid Season</th>
+					<td>£185</td>
+					<td>£148</td>
+					<td>1 May to 30 Jun; 1 to 30 Sep</td>
+				</tr>
+				<tr>
+					<th>Low Season</th>
+					<td>£150</td>
+					<td>£120</td>
+					<td>22 to 30 Apr; 1 to 31 Oct</td>
+				</tr>
+				<tr>
+					<th>Winter Season</th>
+					<td>£110</td>
+					<td>£88</td>
+					<td>6 Jan to 4 Apr; 1 Nov to 19 Dec</td>
+				</tr>
+				<tr>
+					<th>Easter</th>
+					<td>£200</td>
+					<td>£160</td>
+					<td>5 to 21 Apr</td>
+				</tr>
+				<tr>
+					<th>Christmas</th>
+					<td>£200</td>
+					<td>£160</td>
+					<td>20 Dec to 4 Jan</td>
+				</tr>
+			</table>
+		</>
+	);
+}
