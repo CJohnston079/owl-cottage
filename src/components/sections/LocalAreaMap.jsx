23ea7e15b199c8ce0899cadb8bf7e9f641ab84c4
Chris <chris79gp@outlook.com>
2024-10-04 13:00:19 +0100

    Feat: add "Local walks" card to `LocalAreaMap.jsx`

--- src/components/sections/LocalAreaMap.jsx
+++ src/components/sections/LocalAreaMap.jsx
@@ -4,7 +4,8 @@ import CopyWithImg from "../shared/CopyWithImg";
 import Section from "../shared/Section";
 import "../../styles/sections/LocalAreaMap.css";
 
-import lochOich from "../../assets/images/points-of-interest/dog-friendly-restaurants.jpg";
+import doggies from "../../assets/images/points-of-interest/dog-friendly-restaurants.jpg";
+import lochOich from "../../assets/images/points-of-interest/loch-oich.jpg";
 
 export default function LocalAreaMap() {
 	return (
@@ -19,7 +20,7 @@ export default function LocalAreaMap() {
 					style={{ border: "none", borderRadius: "0.25rem" }}
 				></iframe>
 			</Card>
-			<CopyWithImg imgSrc={lochOich} alt="Harbour House pub" emphasis={true} reverse={true}>
+			<CopyWithImg imgSrc={doggies} alt="Harbour House pub" emphasis={true} reverse={true}>
 				<h3>Dog friendly places to eat</h3>
 				<p>
 					The Highlands is very dog friendly and many restaurants and cafes allow dogs, either
@@ -43,6 +44,53 @@ export default function LocalAreaMap() {
 					<li>
 						<a target="_blank" href="https://www.facebook.com/cobbscafe">
 							<Button style="emphasis">Cobbs Café at the Canal, Fort Augustus ➞</Button>
+						</a>
+					</li>
+				</ul>
+			</CopyWithImg>
+			<CopyWithImg imgSrc={lochOich} alt="Harbour House pub" emphasis={true} reverse={true}>
+				<h3>Local walks</h3>
+				<p>
+					Whatever your level of fitness there are many fantastic walks within easy reach of the
+					Cottage. Most are very dog friendly with few roads or livestock around.
+				</p>
+				<p>
+					To find walks that suit you, we suggest looking at the{" "}
+					<a
+						className="external-link"
+						style={{ textDecoration: "underline", color: "var(--cl-emphasis)" }}
+						href="https://forestryandland.gov.scot"
+						target="_blank"
+					>
+						{" "}
+						Forestry and Land Scotland{" "}
+					</a>
+					and{" "}
+					<a
+						className="external-link"
+						style={{ textDecoration: "underline", color: "var(--cl-emphasis)" }}
+						href="https://www.walkhighlands.co.uk/"
+						target="_blank"
+					>
+						Walk Highlands
+					</a>{" "}
+					websites.
+				</p>
+				<p>Some of our favourite walks include:</p>
+				<ul className="button-list">
+					<li>
+						<a target="_blank" href="https://out.ac/IW3IHB">
+							<Button style="emphasis">Loch Oich - Leitirfearn Trail ➞</Button>
+						</a>
+					</li>
+					<li>
+						<a target="_blank" href="https://forestryandland.gov.scot/visit/glengarry">
+							<Button style="emphasis">Glengarry - Allt na Cailliche Trail ➞</Button>
+						</a>
+					</li>
+					<li>
+						<a target="_blank" href="https://forestryandland.gov.scot/visit/torr-dhuin">
+							<Button style="emphasis">Fort Augustus - Torr Dhuin Trail ➞</Button>
 						</a>
 					</li>
 				</ul>
